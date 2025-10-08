6b60436aded49c1457251d2b3a3e8e60148b8e19
mariaakterrimi <mrimi201428@bscse.uiu.ac.bd>
2025-10-08 23:01:01 +0600

    added bet function

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 MAX_LINE = 3        # global var (unchangeable)
+MAX_BET = 100
+MIN_BET = 1
 
 def deposit():
     while True:
@@ -16,7 +18,7 @@ def deposit():
 
 def get_number_of_lines():
     while True:
-        lines = input("How many lines you want to put?: 1-" + str(MAX_LINE) + ")?")
+        lines = input("How many lines you want to put?: (1-" + str(MAX_LINE) + ")?")
         if lines.isdigit():
             lines = int(lines)
             if 1 <= lines <= MAX_LINE:
@@ -24,13 +26,30 @@ def get_number_of_lines():
             else:
                 print("Please enter a line number between 1 to 3.")
         else:
-            print("Please enter a valid line numeber.")
+            print("Please enter a valid line number.")
 
     return lines
+
+def get_bet():
+    while True:
+        amount = input("How much would you like to bet on each line?: $" )
+        if amount.isdigit():
+            amount = int(amount)
+            if MIN_BET <= amount <= MAX_BET:
+                break
+            else:
+                print(f"Amount must be between {MIN_BET} - {MAX_BET}.")     # f string
+        else:
+            print("Please enter a valid number.")
+
+    return amount
 
 def main():
     balance = deposit()
     lines = get_number_of_lines()
+    bet = get_bet()
+    total_bet = bet * lines
+    print(f"You are betting ${bet} on {lines} lines. Total bet is equal to ${total_bet}.")
 
     print(balance, lines)
 
