ccaa85eaa7f76c33b11b51f431231f894ca29ee8
mariaakterrimi <mrimi201428@bscse.uiu.ac.bd>
2025-10-08 19:24:29 +0600

    added line number function

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+MAX_LINE = 3        # global var (unchangeable)
+
 def deposit():
     while True:
         amount = input("Please give an amount: $")
@@ -12,4 +14,22 @@ def deposit():
 
     return amount
 
-deposit()
+def get_number_of_lines():
+    while True:
+        lines = input("How many lines you want to put?: 1-" + str(MAX_LINE) + ")?")
+        if lines.isdigit():
+            lines = int(lines)
+            if 1 <= lines <= MAX_LINE:
+                break
+            else:
+                print("Please enter a line number between 1 to 3.")
+        else:
+            print("Please enter a valid line numeber.")
+
+    return lines
+
+get_number_of_lines()
+def main():
+    balance = deposit()
+
+main()
