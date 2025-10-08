7952f3c8a97f86534b90a665dc3f549835c54849
mariaakterrimi <mrimi201428@bscse.uiu.ac.bd>
2025-10-08 18:27:37 +0600

    added the first deposit function

--- main.py
+++ main.py
@@ -1,1 +1,15 @@
-print("Hello")
+def deposit():
+    while True:
+        amount = input("Please give an amount: $")
+        if amount.isdigit():
+            amount = int(amount)
+            if amount > 0:
+                break
+            else:
+                print("Amount must be greater than 0.")
+        else:
+            print("Please enter a valid amount.")
+
+    return amount
+
+deposit()
