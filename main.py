63bf554a50614e3b694ab60da043aeec41922fcb
mariaakterrimi <mrimi201428@bscse.uiu.ac.bd>
2025-10-09 00:04:40 +0600

    added the spin wheel code

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ COLS = 3
 
 # dictionary
 symbol_count = {
-    "A" : 2,
+    "A" : 2,        # A is there 2 times (symbols : symbol_count)
     "B" : 4,
     "C" : 6,
     "D" : 8
@@ -24,23 +24,24 @@ def get_slotmachine_wheel(rows, cols, symbols):
     columns = []
     for _ in range(cols):
         column = []
-        current_symbols = all_symbols[:]
+        current_symbols = all_symbols[:]        # copy of all_symbols list
         for _ in range(rows):
-            value = random.choice(current_symbols)
-            current_symbols.remove(value)
-            column.append(value)
+            value = random.choice(current_symbols)      # select a random symbol
+            current_symbols.remove(value)               # remove the selected symbol from the list. so if a is selected and there are 2 As then 1 A will be removed
+            column.append(value)                        # add the selected symbol to the column (row actually)
 
         columns.append(column)
 
     return columns
 
 def print_slotmachine(columns):
-    for row in range(len(columns[0])):
+    for row in range(len(columns[0])):          # assuming at least 1 row there
         for i, column in enumerate(columns):
             if i != len(columns) - 1:
-                print(column[row], "|")
+                print(column[row], end=" | ")       # end = newline char
             else:
-                print(column[row])
+                print(column[row], end="")
+        print()
 
 def deposit():
     while True:
